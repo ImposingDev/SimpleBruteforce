46a3e659349cb4b7dd4a86951be279f60075b7dd
ImposingDev <31015281+ImposingDev@users.noreply.github.com>
2017-08-27 20:55:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,19 +7,26 @@ namespace Bruteforce
  
         static void Main(string[] args)
         {
+            //Set Title
             Console.Title = "Simple Local Bruteforce";
             Console.WriteLine("|--Write a word to bruteforce!--|");
+            //Start the Bruteforce
             Console.WriteLine("[-Finished-] All chars found! - Password Found: {0}", BruteForcer());
             for (int i = 0; i < 100; i++)
+            //for not automatically closing
             Console.ReadKey();
 
         }
         static string BruteForcer()
         {
+            //read the word
             string word = Console.ReadLine();
+            //all chars
             string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"/\\.,-|+#*'§$%&()=?´<> ";
             string result = "";
-            Console.WriteLine("[+][+] Starting BruteForce...");
+            
+            Console.WriteLine("[WORKING] Starting BruteForce...");
+            //BRUTEFORCE
             for (int x = 0; x <= word.Length - 1; x++)
             {
                 foreach (char ch in chars)
@@ -27,6 +34,7 @@ namespace Bruteforce
                     char pch = Convert.ToChar(word[x]);
                     if (ch == pch)
                     {
+                        //if match
                         Console.WriteLine("[Try] Trying: {0}", ch);
                         Console.WriteLine("[Found] Found char: {0}", ch);
                         result += ch;
@@ -34,12 +42,14 @@ namespace Bruteforce
                     }
                     else
                     {
+                        //if not matched
                         Console.WriteLine("[Try] Still Trying: {0}", ch);
 
                     }
 
                 }
             }
+            //result
             return result;
         }
     }
